708afff9abb9d35e255c347069109b35bcf96c97
Chaser90 <gyuhan1132@gmail.com>
2020-05-21 16:44:47 +0900

    Eclipse use to study

---
+++ src/javaStudy/PrimitiveDataTypeExam.java
@@ -0,0 +1,22 @@
+package javaStudy;
+
+public class PrimitiveDataTypeExam {
+
+	public static void main(String[] args) {
+		boolean isFun = false;
+		System.out.print(isFun);
+
+		
+		char c = 'f';
+		
+		int x = 59;
+		
+		long bing = 345455433L;
+		
+		float f = 32.4F;
+		
+		double d = 3434344.5;
+		
+	}
+
+}
